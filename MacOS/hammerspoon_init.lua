756d68fa8b1e5200337730cab2e1e3305daf53fb
alex-ong <the.onga@gmail.com>
2021-05-14 11:11:19 +1000

    Add almost full bindings for hammerspoon

--- MacOS/hammerspoon_init.lua
+++ MacOS/hammerspoon_init.lua
@@ -17,10 +17,49 @@ f18 = hs.hotkey.bind({}, 'F18', enterHyperMode, exitHyperMode)
 
 
 bindings = {}
+-- wasd
 bindings['s'] = 'down'
 bindings['a'] = 'left'
 bindings['w'] = 'up'
 bindings['d'] = 'right'
+-- ijkl
+bindings['i'] = 'up'
+bindings['j'] = 'left'
+bindings['k'] = 'down'
+bindings['l'] = 'right'
+
+-- home end etc
+bindings['u'] = 'pageup'
+bindings['o'] = 'pagedown'
+
+bindings['h'] = 'home'
+bindings['n'] = 'end'
+
+
+-- Function keys
+bindings['1'] = 'f1'
+bindings['2'] = 'f2'
+bindings['3'] = 'f3'
+bindings['4'] = 'f4'
+bindings['5'] = 'f5'
+bindings['6'] = 'f6'
+bindings['7'] = 'f7'
+bindings['8'] = 'f8'
+bindings['9'] = 'f9'
+bindings['0'] = 'f10'
+bindings['-'] = 'f11'
+bindings['='] = 'f12'
+
+-- Command keys idk
+bindings['p'] = 'printscreen'
+bindings['['] = 'scrolllock'
+bindings[']'] = 'pause'
+bindings[';'] = 'insert'
+bindings[','] = 'volume_down'
+bindings['.'] = 'volume_up'
+bindings['/'] = 'volume_mute'
+
+
 for binding, key in pairs(bindings) do
   print(binding, key)
   hyper:bind({}, binding,
@@ -31,8 +70,12 @@ for binding, key in pairs(bindings) do
 end
 
 
-
+--TODO: 
+-- At the moment, holding down a key doesnt cause autorepeat
+-- Also: [shift/ctrl] + [caps i] won't 
 --release_down = hs.eventtap.event.newKeyEvent({},'down',false)
+--release_down = hs.eventtap.event.newKeyEvent({},'down',true)
+
 
 --hyper:bind('', 'J', 
 --function() hs.eventtap.keyStroke({},'down') end)
